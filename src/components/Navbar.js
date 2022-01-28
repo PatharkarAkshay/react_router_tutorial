1444ea87dabba5e62f549880762ec062825513b0
Akshay P <akshayp@colossusnexus.com>
2022-01-28 16:17:01 +0530

    Links and Active Links

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+  const navLinkStyles = ({ isActive }) => {
+    return {
+      fontWeight: isActive ? "bold" : "normal",
+      textDecoration: isActive ? "none" : "underline",
+    };
+  };
+
+  return (
+    <nav className="primary-nav">
+      <NavLink to="/" style={navLinkStyles}>
+        Home
+      </NavLink>
+      <NavLink to="/about" style={navLinkStyles}>
+        About
+      </NavLink>
+    </nav>
+  );
+};
